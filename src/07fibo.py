de80f47f42ccb69e5b283e515c8956027b54fd5f
t.onodera <t.onodera.1.2.1.5@gmail.com>
2022-11-17 21:37:16 +0900

    フィボナッチ数列のN項目を求めるプログラムをメモ化

--- src/07fibo.py
+++ src/07fibo.py
@@ -1,13 +1,23 @@
-def fibo(N: int) -> int:
-    print("再起関数を呼び出しました: ", N)
-    if N == 0:
-        return 0
-    elif N == 1:
-        return 1
+class Fibo:
+    def __init__(self):
+        self.memo = []
 
-    result = fibo(N - 1) + fibo(N - 2)
-    print(N)
-    return result
+    def fibo(self, N: int) -> int:
+
+        if len(self.memo) == 0:
+            self.memo = [-1 for _ in range(N + 1)]
+
+        if N == 0:
+            return 0
+        elif N == 1:
+            return 1
+        if self.memo[N] != -1:
+            return self.memo[N]
+
+        self.memo[N] = self.fibo(N - 1) + self.fibo(N - 2)
+        return self.memo[N]
 
 
-print(fibo(4))
+f = Fibo()
+
+print(f.fibo(100))
