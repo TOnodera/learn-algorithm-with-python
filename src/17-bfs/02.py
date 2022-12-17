697b5e67040b7364539d19603c8779ed027e0239
t.onodera <t.onodera.1.2.1.5@gmail.com>
2022-12-17 22:26:22 +0900

    コメント追加

--- src/17-bfs/02.py
+++ src/17-bfs/02.py
@@ -15,12 +15,16 @@ dist[0] = 0
 
 que.put(0)
 
+# キューに次に探索するべき頂点が入っているので
+# 探索する頂点がなくなるまでループする
 while not que.empty():
     v = que.get()
-
+    # その頂点からいける（たどれる）頂点を一個ずつ調べる
     for next_v in G[v]:
         if dist[next_v] != -1:
             continue
 
+        # その頂点に何手目で到達できるか記録する
         dist[next_v] = dist[v] + 1
+        # 次に探索する頂点を入れておく
         que.put(next_v)
