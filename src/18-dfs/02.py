7772331335ce2284c1ed790524f5ab64064f44ca
t.onodera <t.onodera.1.2.1.5@gmail.com>
2022-12-18 23:48:43 +0900

    DFSを再帰的に実装

---
+++ src/18-dfs/02.py
@@ -0,0 +1,24 @@
+def dfs(G, v):
+    for next_v in G[v]:
+        if dist[next_v] != -1:
+            continue
+        dist[next_v] = dist[v] + 1
+        dfs(G, next_v)
+
+
+# DFS
+N, M = map(int, input().split())
+G = [[] for i in range(N)]
+for i in range(M):
+    A, B = map(int, input().split())
+    G[A].append(B)
+    G[B].append(A)
+
+dist = [-1] * N
+dist[0] = 0
+
+
+dfs(G, 0)
+
+
+print(dist)
